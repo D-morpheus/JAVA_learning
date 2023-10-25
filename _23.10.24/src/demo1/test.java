7c28b1524757317e2929823d7e8f5a99b72f40eb
Mor <108830742+D-morpheus@users.noreply.github.com>
2023-10-25 21:10:32 +0800

    Add files via upload

---
+++ _23.10.24/src/demo1/test.java
@@ -0,0 +1,10 @@
+package demo1;
+
+public class test {
+    public static void main(String[] args) {
+        Object obj = new Object();
+        String str1 = obj.toString();
+        System.out.println(str1);
+
+    }
+}
---
+++ _23.10.24/src/demo2/test.java
@@ -0,0 +1,20 @@
+package demo2;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class test {
+    public static void main(String[] args) {
+//        BigInteger bd1 = new BigInteger(20, new Random());
+//        System.out.println(bd1);
+
+//        BigInteger bd2 = new BigInteger("1", 100);
+//        System.out.println(bd2);
+
+//        BigInteger bd3 = BigInteger.valueOf(100);
+//        System.out.println(bd3);
+
+        BigInteger bd4 = new BigInteger("999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999");
+        System.out.println(bd4);
+    }
+}
